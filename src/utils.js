900136973ea2da2be46b21664866c298cd8f0c0f
권기범 <kgbum2222@gmail.com>
2019-07-31 00:06:29 +0900

    requestSecret Resolver

---
+++ src/utils.js
@@ -0,0 +1,9 @@
+import {
+  adjectives,
+  nouns,
+} from './words';
+
+export const generateSecret = () => {
+  const randomNumber = Math.floor(Math.random() * adjectives.length);
+  return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
+};
