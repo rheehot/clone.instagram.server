0c2078d3afe96a611db0f0044a136f166fd9a386
권기범 <kgbum2222@gmail.com>
2019-09-14 11:32:18 +0900

    toggleLike Resolver

---
+++ src/middlewares.js
@@ -0,0 +1,6 @@
+/* eslint-disable import/prefer-default-export */
+export const isAuthenticated = (request) => {
+  if (!request.user) {
+    throw Error('You need to log in to perform this action');
+  }
+};
