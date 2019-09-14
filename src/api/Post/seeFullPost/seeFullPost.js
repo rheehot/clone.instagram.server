ba7455477fe43e56e299c89af445473576fdd58b
권기범 <kgbum2222@gmail.com>
2019-09-14 17:02:49 +0900

    upload Resolver

--- src/api/Post/seeFullPost/seeFullPost.js
+++ src/api/Post/seeFullPost/seeFullPost.js
@@ -24,10 +24,19 @@ export default {
           },
         },
       }).aggregate().count();
+      const files = await prisma.post({
+        id,
+      }).files();
+      const user = await prisma.post({
+        id,
+      })
+        .user();
       return {
         post,
+        files,
         comments,
         likeCount,
+        user,
       };
     },
   },
