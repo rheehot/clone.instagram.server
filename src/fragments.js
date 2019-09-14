91babe945c22ef5bb17d9cc48078aef3c6ec923f
권기범 <kgbum2222@gmail.com>
2019-09-14 17:32:08 +0900

    seeFullPost Refactoring

--- src/fragments.js
+++ src/fragments.js
@@ -1,10 +1,50 @@
 /* eslint-disable import/prefer-default-export */
+export const USER_FRAGMENT = `
+  fragment UserParts on User {
+    id
+    username    
+  }
+`;
+
 export const COMMENT_FRAGMENT = `
 fragment CommentParts on Comment {
   id
   text
   user
   {
-    username
+    id
+    username    
   }
 }`;
+
+export const FILE_FRAGMENT = `
+  fragment FileParts on File {
+    id
+    url
+  }
+`;
+
+export const FULL_POST_FRAGMENT = `
+  fragment PostParts on Post {
+    id
+    location
+    caption
+    files {
+      id
+    url
+    }
+    comments {
+      id
+  text
+  user
+  {
+    id
+    username    
+  }
+    }
+    user {
+      id
+    username    
+    }
+  }
+`;
