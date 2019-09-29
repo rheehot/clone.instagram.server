2a768afc21a98056be446915a0a126702b24c20c
권기범 <kgbum2222@gmail.com>
2019-09-29 18:54:23 +0900

    createAccount error 처리

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -5,6 +5,12 @@ export default {
   Mutation: {
     createAccount: async (_, args) => {
       const notNull = cleanNullArgs(args);
+      const exists = await prisma.$exists.user({
+        OR: [{ username: notNull.username }, { email: notNull.email }],
+      });
+      if (exists) {
+        throw Error('This username / email is already taken');
+      }
       await prisma.createUser({
         ...notNull,
       });
